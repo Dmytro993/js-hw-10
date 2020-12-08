b648f51c5f60a9028c655215a49d6814f18591bb
Dmytro Nepyivoda <dimanepyivoda246@gmail.com>
2020-12-08 11:23:11 +0200

    myThemePage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import css from "./styles.css";
+import hbs from "../templates/post.hbs"
+import menu from "../src/menu.json"
+
+
+
+const ul = document.querySelector(".js-menu")
+const myButton = document.querySelector("#theme-switch-toggle")
+const body = document.querySelector("body")
+
+ul.insertAdjacentHTML("beforeend", hbs(menu))
+
+
+myButton.addEventListener("change", e =>{
+   if(e.target.checked){
+      body.classList.remove("light-theme")
+      body.classList.add("dark-theme")
+      localStorage.setItem("my-theme", "dark")
+   }
+   if(!e.target.checked){
+      body.classList.remove("dark-theme")
+      body.classList.add("light-theme")
+      localStorage.setItem("my-theme", "light")
+   }
+})
+ 
+function localStore(){
+    const theme = localStorage.getItem("my-theme")
+    if(theme === "dark"){
+        body.classList.add("dark-theme")
+        myButton.checked = true;
+    }
+    if(theme === "light"){
+        body.classList.add("light-theme")
+        myButton.checked = false;
+    }
+}
+localStore()
+
+
