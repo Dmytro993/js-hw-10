3e1917b38648b8cb046592a179bb9754ed4a6d5c
Dmytro Nepyivoda <dimanepyivoda246@gmail.com>
2020-12-08 17:24:06 +0200

    changeFunction

--- src/index.js
+++ src/index.js
@@ -1,48 +1,46 @@
 import "./styles.css";
-import hbs from "./templates/post.hbs"
-import menu from "./menu.json"
+import hbs from "./templates/post.hbs";
+import menu from "./menu.json";
 
+const ul = document.querySelector(".js-menu");
+const myButton = document.querySelector("#theme-switch-toggle");
+const body = document.querySelector("body");
 
+ul.insertAdjacentHTML("beforeend", hbs(menu));
 
-const ul = document.querySelector(".js-menu")
-const myButton = document.querySelector("#theme-switch-toggle")
-const body = document.querySelector("body")
+myButton.addEventListener("change", myListener);
 
-ul.insertAdjacentHTML("beforeend", hbs(menu))
-
-
-myButton.addEventListener("change", e =>{
-   if(e.target.checked){
-      removeClass("light-theme")
-      addClass("dark-theme")
-      setMyItem("my-theme", "dark")
-   }
-   if(!e.target.checked){
-      removeClass("dark-theme")
-      addClass("light-theme")
-      setMyItem("my-theme", "light")
-   }
-})
- 
-function localStore(){
-    const theme = localStorage.getItem("my-theme")
-    if(theme === "dark"){
-        addClass("dark-theme")
-        myButton.checked = true;
-    }
-    if(theme === "light"){
-        addClass("light-theme")
-        myButton.checked = false;
-    }
+function localStore() {
+  const theme = localStorage.getItem("my-theme");
+  if (theme === "dark") {
+    addClass("dark-theme");
+    myButton.checked = true;
+  }
+  if (theme === "light") {
+    addClass("light-theme");
+    myButton.checked = false;
+  }
 }
- localStore()
+localStore();
 
-function addClass( myClass){
-return body.classList.add(myClass)
+function addClass(myClass) {
+  return body.classList.add(myClass);
 }
-function removeClass(reClass){
-return body.classList.remove(reClass)
+function removeClass(reClass) {
+  return body.classList.remove(reClass);
 }
-function setMyItem (key , value){
-return localStorage.setItem(key, value)
+function setMyItem(key, value) {
+  return localStorage.setItem(key, value);
+}
+function myListener(e) {
+  if (e.target.checked) {
+    removeClass("light-theme");
+    addClass("dark-theme");
+    setMyItem("my-theme", "dark");
+  }
+  if (!e.target.checked) {
+    removeClass("dark-theme");
+    addClass("light-theme");
+    setMyItem("my-theme", "light");
+  }
 }
